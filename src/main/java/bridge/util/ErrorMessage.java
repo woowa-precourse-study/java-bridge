b6401f6e80172083260d77387082e8c294ac30ce
Tae Kyoun Kim <xorbskim6105@gmail.com>
2025-12-18 17:32:13 +0900

    feat : 에러 메시지 추가

--- src/main/java/bridge/util/ErrorMessage.java
+++ src/main/java/bridge/util/ErrorMessage.java
@@ -2,4 +2,6 @@ package bridge.util;
 
 public class ErrorMessage {
     public static final String INPUT_RANGE_ERROR = "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.";
+    public static final String INPUT_SELECT_ERROR = "[ERROR] 칸은 U 또는 D로 입력해주세요.";
+    public static final String INPUT_RETRY_ERROR = "[ERROR] 재시작 여부는 R 또는 Q로 입력해주세요.";
 }
